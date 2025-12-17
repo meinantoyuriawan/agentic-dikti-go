9b634640486ccd9bc4e61a0f6baed6c5248afcbf
Meinanto Tri Yuriawan <45266362+meinantoyuriawan@users.noreply.github.com>
2025-12-17 22:18:18 +0700

    feat: chat handler implementation, timestamp for user and ai chat log not yet

--- internal/server/chat_handler.go
+++ internal/server/chat_handler.go
@@ -1,13 +1,23 @@
 package server
 
 import (
+	"AgenticDikti/internal/model"
 	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/google/uuid"
 )
 
+func generateDeterministicUUID(sessionId, userMessage string) string {
+	data := fmt.Sprintf(`%s-%s-%s`, sessionId, userMessage, time.Now().UTC().Unix())
+	return uuid.NewSHA1(uuid.NameSpaceURL, []byte(data)).String()
+}
+
 func (s *Server) chatHandler(w http.ResponseWriter, r *http.Request) {
-	// ctx := r.Context()
+	ctx := r.Context()
 	var req DiktiRequest
 	err := json.NewDecoder(r.Body).Decode(&req)
 	if err != nil {
@@ -23,12 +33,67 @@ func (s *Server) chatHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	// chat, err := s.service.GetChatHistory(ctx, req.SessionId)
-	// if err != nil {
-	// 	log.Printf("error getting chat history: %s\n", err.Error())
-	// 	sendErrorResponse(w, http.StatusInternalServerError, "Terjadi kesalahan, Mohon input ulang")
-	// 	return
-	// }
+	// generate chat id
+	// GetChatHistory
+	// transform chat history to string format of
+	// - User:
+	// - Assistant:
+	// hit RunAgent(ctx, convstring, req.chatinput)
+
+	// set the return of agent as ai, chatinput as user and save both use chat id
+	// call InputChat
+
+	// implementation
+
+	chat, err := s.service.GetChatHistory(ctx, req.SessionId)
+	if err != nil {
+		log.Printf("error getting chat history: %s\n", err.Error())
+		sendErrorResponse(w, http.StatusInternalServerError, "Terjadi kesalahan, Mohon input ulang")
+		return
+	}
+
+	history := ``
+
+	for _, d := range chat {
+		chatRole := `User`
+		if d.Role.String == "ai" {
+			chatRole = `Assistant`
+		}
+		history += fmt.Sprintf(`%s : %s \n`, chatRole, d.ChatInput.String)
+	}
+
+	agentResponse, err := s.service.RunAgent(ctx, history, req.ChatInput)
+	if err != nil {
+		log.Printf("error hit agent: %s\n", err.Error())
+		sendErrorResponse(w, http.StatusInternalServerError, "Terjadi kesalahan, Mohon input ulang")
+		return
+	}
+
+	userChat := model.ChatLogs{
+		// req.SessionId,
+		// generateDeterministicUUID(req.SessionId, req.ChatInput),
+		// req.ChatInput,
+		// "timestamp",
+		// "user",
+		// false,
+		// req.UniversityId,
+	}
+	aiChat := model.ChatLogs{
+		// req.SessionId,
+		// generateDeterministicUUID(req.SessionId, agentResponse),
+		// agentResponse,
+		// "timestamp",
+		// "ai",
+		// false,
+		// req.UniversityId,
+	}
+
+	err = s.service.InputChat(ctx, userChat, aiChat)
+	if err != nil {
+		log.Printf("error saving chat: %s\n", err.Error())
+		sendErrorResponse(w, http.StatusInternalServerError, "Terjadi kesalahan, Mohon input ulang")
+		return
+	}
 
 	//agent calling
 	resp := DiktiResponse{
--- internal/server/server.go
+++ internal/server/server.go
@@ -16,6 +16,7 @@ type Service interface {
 	GetChatHistory(ctx context.Context, sessionId string) (chat []model.ChatHistory, err error)
 	InputChat(ctx context.Context, userLog model.ChatLogs, aiLog model.ChatLogs) (err error)
 	InputBooking(ctx context.Context, userBookData model.BookingData) (err error)
+	RunAgent(ctx context.Context, conversationString, userQuestion string) (string, error)
 }
 
 type Server struct {
